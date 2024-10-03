e0217a98aa7048d05ff7cc8db2fb6702d4feaaec
tuheen66 <hassan.monirul@gmail.com>
2024-10-04 03:36:33 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,8 +35,45 @@ async function run() {
       .db("tourismo")
       .collection("guide_profile");
 
-    app.get("/user", async (req, res) => {
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
+
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify (token, process.nextTick.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: " forbidden access"})
+        }
+        req.decoded=decoded;
+        next()
+      })
+      
+      
+    };
+
+    app.get("/user", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
@@ -89,7 +127,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/user/:email", async (req, res) => {
+    app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
 
       const query = { email: email };
@@ -100,15 +138,9 @@ async function run() {
       let admin = false;
 
       if (user) {
-        admin = user.role === "admin";
+        admin = user?.role === "admin";
       }
       res.send({ admin });
-    });
-
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await userCollection.insertOne(user);
-      res.send(result);
     });
 
     app.get("/packages", async (req, res) => {
